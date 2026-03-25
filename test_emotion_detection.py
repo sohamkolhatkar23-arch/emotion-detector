5656848e98f59c1094a890d35531724b0dcebc07
sohamkolhatkar23-arch <soham.kolhatkar23@vit.edu>
2026-03-25 16:55:49 +0530

    Create test_emotion_detection.py

---
+++ test_emotion_detection.py
@@ -0,0 +1,19 @@
+import unittest
+from EmotionDetection.emotion_detection import emotion_detector
+
+class TestEmotionDetector(unittest.TestCase):
+
+    def test_joy(self):
+        result = emotion_detector("I am happy")
+        self.assertEqual(result['dominant_emotion'], 'joy')
+
+    def test_anger(self):
+        result = emotion_detector("I am angry")
+        self.assertEqual(result['dominant_emotion'], 'anger')
+
+    def test_sadness(self):
+        result = emotion_detector("I am sad")
+        self.assertEqual(result['dominant_emotion'], 'sadness')
+
+if __name__ == "__main__":
+    unittest.main()
